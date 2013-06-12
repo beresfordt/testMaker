068039bf0e9b6cb2c7ffd30738797c1cdce321f4
Tom Beresford <github@tomberesford.co.uk>
2013-06-12 23:50:25 +0100

    bedtime - failing test to pick up tomorrow

--- t/tests/Test/TestMaker/TestSub.pm
+++ t/tests/Test/TestMaker/TestSub.pm
@@ -8,12 +8,31 @@ sub constructor : Tests() {
     my $test = shift;
 
     can_ok($test->class(), 'new');
+
+    dies_ok {
+        $test->class->new()
+    } '.... dies with no args';
+
+    dies_ok {
+        $test->class->new({subName => undef})
+    } '.... dies with subName undef';
+
+    lives_ok {
+        $test->class->new({subName => 'new'})
+    } '.... lives with subName as new';
 }
 
 sub subName : Tests() {
     my $test = shift;
 
     can_ok($test->class(), 'subName');
+
+    my $testSub = $test->testSubFactory('blah');
+    is($testSub->subName(), 'blah', '.... we return the expected subName after construction');
+
+    dies_ok {
+        $testSub->subName(undef)
+    } '.... dies if we try and set subName to undef';
 }
 
 sub testSubName : Tests() {
@@ -32,6 +51,16 @@ sub testSubDefinition : Tests() {
     my $test = shift;
 
     can_ok($test->class(), 'testSubDefinition');
+}
+
+sub testSubFactory {
+    my ($test, $subName) = @_;
+    if ($subName) {
+        return $test->class->new({subName => $subName});
+    }
+    else {
+        return $test->class->new({subName => 'testSub'});
+    }
 }
 
 1;
